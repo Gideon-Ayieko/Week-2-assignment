e31aa1600f18bf690068303588acc2722a764e85
Gideon-Ayieko <goaonduru@gmail.com>
2023-10-10 15:41:07 +0300

    First Commit

---
+++ .idea/assignment.js
@@ -0,0 +1,21 @@
+//ASSIGNMENT FOR WEEK 2
+//EXPLICIT CONVERSION
+
+//Question 1 - Convert to Number
+let numberStr = "42";
+numberStr = Number(numberStr);//Converting String to Number
+console.log(numberStr);
+
+//Question 2 - Convert to String
+let num = 2023;
+num = String(num)//Converting Number to String
+console.log(num);
+
+//Qustion 3 - Convert to Boolean
+let truthyValue = "Hello";
+let falsyValue = 0;
+truthyValue = Boolean(truthyValue); //Converting String to Boolean
+falsyValue = Boolean(falsyValue); //Converting to Boolean
+console.log(truthyValue);
+console.log(falsyValue);
+
